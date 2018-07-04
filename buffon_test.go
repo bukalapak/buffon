a6d15ac5f7c65f5eecdbaee97707c10f7a2f5ade
Alif Rachmawadi <subosito@bukalapak.com>
2018-07-04 10:24:19 +0700

    split tests

--- buffon_test.go
+++ buffon_test.go
@@ -96,26 +96,6 @@ func TestAggregator(t *testing.T) {
 	})
 }
 
-func TestDefaultExecutor(t *testing.T) {
-	t.Run("invalid-backend", func(t *testing.T) {
-		exc, err := buffon.NewDefaultExecutor("http:// invalid")
-		assert.NotNil(t, err)
-		assert.Nil(t, exc)
-	})
-}
-
-func TestError(t *testing.T) {
-	assert.Equal(t, "error", buffon.Error{Message: "error"}.Error())
-}
-
-func TestErrorMulti(t *testing.T) {
-	errs := make(buffon.ErrorMulti)
-	errs["foo"] = errors.New("foo")
-	errs["bar"] = errors.New("bar")
-
-	assert.ElementsMatch(t, []string{"foo", "bar"}, strings.Split(errs.Error(), ","))
-}
-
 func handler() http.Handler {
 	m := pat.New()
 
---
+++ default_test.go
@@ -0,0 +1,30 @@
+package buffon_test
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/bukalapak/buffon"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultExecutor(t *testing.T) {
+	t.Run("invalid-backend", func(t *testing.T) {
+		exc, err := buffon.NewDefaultExecutor("http:// invalid")
+		assert.NotNil(t, err)
+		assert.Nil(t, exc)
+	})
+}
+
+func TestError(t *testing.T) {
+	assert.Equal(t, "error", buffon.Error{Message: "error"}.Error())
+}
+
+func TestErrorMulti(t *testing.T) {
+	errs := make(buffon.ErrorMulti)
+	errs["foo"] = errors.New("foo")
+	errs["bar"] = errors.New("bar")
+
+	assert.ElementsMatch(t, []string{"foo", "bar"}, strings.Split(errs.Error(), ","))
+}
